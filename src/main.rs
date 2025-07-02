0daa3e0a1578b60ee9ac8eaac1832fa41c2d612b
Rithvik Redrouthu <rithvik.redrouthu@gmail.com>
2025-07-02 12:35:07 -0400

    draw sphere

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,26 @@ mod ray;
 use crate::{
     color::Color, color::write_color,
     ray::Ray,
-    vec3::{Vec3, unit_vector},
+    vec3::{Vec3, Point3, unit_vector, dot},
 };
 use indicatif::{ProgressBar, ProgressStyle};
 
+pub fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> bool {
+    let oc = center - r.origin();
+
+    let a = dot(&r.direction(), &r.direction());
+    let b = dot(&r.direction(), &oc) * 2.0;
+    let c = dot(&oc, &oc) - radius * radius;
+
+    let discriminant = (b * b) - (4.0 * a * c);
+    discriminant >= 0.0
+}
+
 pub fn ray_color(r: &Ray) -> Color{
+    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r) {
+        return Color::new(1.0, 0.0, 0.0);
+    }
+
     let unit_direction = unit_vector(r.direction());
     let a = 0.5 * (unit_direction.y() + 1.0);
     Color::new(1.0, 1.0, 1.0) * (1.0 - a) + Color::new(0.5, 0.7, 1.0) * a
