ccbefbf7d239d3375106f04213e9f81eaefeb05d
Rithvik Redrouthu <rithvik.redrouthu@gmail.com>
2025-07-16 19:33:45 -0400

    gamma correction

--- src/color.rs
+++ src/color.rs
@@ -3,10 +3,17 @@ use crate::vec3::Vec3;
 
 pub type Color = Vec3;
 
+pub fn linear_to_gamma(linear_component: f64) -> f64 {
+    if linear_component > 0.0 {
+        return linear_component.sqrt();
+    }
+    return 0.0;
+}
+
 pub fn write_color(pixel_color: &Color) {
-    let r = pixel_color.x();
-    let g = pixel_color.y();
-    let b = pixel_color.z();
+    let r = linear_to_gamma(pixel_color.x());
+    let g = linear_to_gamma(pixel_color.y());
+    let b = linear_to_gamma(pixel_color.z());
 
     const INTENSITY: Interval = Interval::new(0.000, 0.999);
     let rbyte = (INTENSITY.clamp(r) * 256.0) as u8;
